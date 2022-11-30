7931e91b3c32e15de576fa47676da93ffb15a5c7
cmacha2 <cristiancmg127@gmail.com>
2022-11-30 12:55:34 -0800

    add Server IP

--- src/utils/checkStatusCase.js
+++ src/utils/checkStatusCase.js
@@ -1,8 +1,8 @@
-import { URL_SERVER } from "@env";
+
 
 export const checkStatus = async (numberCase) => {
   try {
-    const response = await fetch(`http://52.2.236.126:3000`, {
+    const response = await fetch(`http://3.87.52.235:3000`, {
       method: "POST", // or 'PUT'
       headers: {
         "Content-Type": "application/json",
